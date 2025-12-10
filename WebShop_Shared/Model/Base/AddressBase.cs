75e37cd2cd842e207e6fce895b8df404d58aaff4
Ivan Radoš <ivan@neostar.com>
2025-12-10 18:55:42 +0100

    update

--- WebShop_Shared/Model/Base/AddressBase.cs
+++ WebShop_Shared/Model/Base/AddressBase.cs
@@ -1,10 +1,16 @@
-﻿namespace WebShop_Shared.Model.Base
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShop_Shared.Model.Base
 {
     public abstract class AddressBase
     {
+        [Display(Name = "Ulica")]
         public string Street { get; set; }
+        [Display(Name = "k.Broj")]
         public string Number { get; set; }
+        [Display(Name = "Grad")]
         public string City { get; set; }
+        [Display(Name = "Država")]
         public string Country { get; set; }
     }
 }
